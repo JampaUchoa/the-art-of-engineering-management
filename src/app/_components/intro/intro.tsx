92327339d8fe5c11088e140968719c84ee614183
Jampa Uchoa <joaopcu@gmail.com>
2025-04-11 02:50:50 -0300

    add hero

--- src/app/_components/intro/intro.tsx
+++ src/app/_components/intro/intro.tsx
@@ -1,28 +1,33 @@
 import tableOfContents from "@/lib/toc";
 import "./intro.scss";
 import "../chapter/navigator.scss";
-import { FiBookOpen } from "react-icons/fi";
-
+import hero from "./hero.svg"
 export default function Intro() {
   const chapter1 = tableOfContents[0].subtopics[0];
 
   return (
-    <div className="home-page">
-      <div className="intro-text">
-        <h2>
-          Build high-impact teams, with a practical approach.
-        </h2>
-        <p>
-          Learn effective strategies to master engineering management and deliver great products.
-        </p>
+    <div className="intro">
+      <div>
+        <div className="intro-text">
+          <h2>
+            Build high-impact teams, with a practical approach.
+          </h2>
+          <p>
+            Learn effective strategies to master engineering management and deliver great products.
+          </p>
+        </div>
+        <div className="buttons intro-text">
+          <a className="get-started" href={`/chapters/${chapter1.slug}`}>
+            <div>
+              <span className="chapter-title">Read &gt;</span>
+            </div>
+          </a>
+        </div>
       </div>
-      <div className="buttons intro-text">
-        <a className="get-started" href={`/chapters/${chapter1.slug}`}>
-          <div>
-            <span className="chapter-title">Read &gt;</span>
-          </div>
-        </a>
+      <div className="hero">
+        <img src={hero.src}/>
       </div>
+
     </div>
   );
 }
